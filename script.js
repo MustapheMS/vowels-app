021a1cd51a4fa672aa5715ed19a3520aecf443e7
MustapheMS <moztaphem9@gmail.com>
2023-06-26 14:17:06 +0300

    Vowels Counter App

--- script.js
+++ script.js
@@ -1,1 +1,20 @@
+// Done Vowels Counter 💯💯😎😎
 
+const inputText = document.getElementById("input-text");
+const countButton = document.getElementById("count-btn");
+const vowelCount = document.getElementById("vowel-count");
+
+function countVowels() {
+  const vowelss  = inputText.value.toLowerCase();
+  const vowels = ["a", "e", "i", "o", "u"];
+  let count = 0;
+
+  for (let i = 0; i < vowelss.length; i++) {
+    if (vowels.includes(vowelss [i])) {
+      count++;
+    }
+  }
+
+  vowelCount.textContent = count;
+}
+countButton.addEventListener("click", countVowels);
